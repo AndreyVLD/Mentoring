2602b7da1f3134a3cfd44fdf73d5a4f30deded6e
AndreyVLD <nicula2003@gmail.com>
2023-01-23 22:25:33 +0100

    First Steps

--- JS/Validate.js
+++ JS/Validate.js
@@ -1,12 +1,20 @@
+
 function validateForm(){
     let uid = document.MenteeForm.user_name.value;
     let validUserName = validateUserName(uid);
 
 
-    document.getElementById("demo").innerHTML = uid;
 }
 
-function validateUserName(UserName){
-    
+function validateUserName(username){
+    if(username==""){
+        
+        document.getElementById("unError").classList.add('error');
+        document.getElementById("unError").innerHTML = "The user name is required!";
+    }
+    else{
+        document.getElementById("unError").innerHTML = "Everything looks great!";
+        document.getElementById("unError").classList.replace('error','correct');
+    }
 
 }
