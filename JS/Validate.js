03f4201f8860820f6ad1c87cf4e1399c897dcfc7
AndreyVLD <nicula2003@gmail.com>
2023-01-24 01:00:14 +0100

    Huge update on regex

--- JS/Validate.js
+++ JS/Validate.js
@@ -1,24 +1,99 @@
-
 function validateForm(){
     let uid = document.MenteeForm.user_name.value;
+
     let validUserName = validateUserName(uid);
 
+    let pas = document.MenteeForm.password.value;
+    let confPass = document.MenteeForm.confirm_password.value;
+
+    let validPassword =  validatePassword(pas, confPass);
+
+    let nam = document.MenteeForm.name.value;
 
 }
 
+
+
+function validatePassword(password, confirmPassword){
+    let size = /^\S{12,}$/;
+    let format = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)/;
+    if(password==""){
+        hide("passConfError");
+        incorrectInput("passError","Password is required!");
+        return false;
+
+    }else if(! size.test(password)){
+        hide("passConfError");
+        incorrectInput("passError","Password must be at least 12 characters long!");
+        return false;
+
+    }
+    else if(! (format.test(password))){
+
+        incorrectInput("passError","Password must containt at least: 1 uppercase letter, 1 lowercase letter, 1 number, 1 special character!");
+        hide("passConfError");
+        return false;
+
+    }else if(password != confirmPassword){
+        unhide("passConfError");
+        correctInput("passError");
+        incorrectInput("passConfError","Passwords do not match!");
+        return false;
+
+    }else{
+        unhide("passConfError");
+        correctInput("passError");
+        correctInput("passConfError");
+        return true;
+
+    }
+}
+
+
+
 function validateUserName(username){
-    const regex = new RegExp('');
-    if(username=="" || ! regex.test(username)){
-        document.getElementById("unError").classList.remove('correct');
-        document.getElementById("unError").classList.add('error');
-        document.getElementById("unError").innerHTML = "The user name is required!";
+    let size = /^\S{5,12}$/;
+    let first = /^[A-Z]/;
+    let last  = /[^A-Za-z]{1}$/;
+    if(username==""){
+        incorrectInput("unError","Username is required!");
+        return false;
+    }
+    else if(! size.test(username)){
+        incorrectInput("unError","Username must be of length 5 to 12!");
+        return false;
+
+    }else if(! (first.test(username) && last.test(username))){
+        incorrectInput("unError","Username must start with capital letter and end with a number or special character!");
         return false;
     }
     else{
-        document.getElementById("unError").classList.remove('error');
-        document.getElementById("unError").classList.add('correct');
-        document.getElementById("unError").innerHTML = "Everything looks great!";
+        correctInput("unError");
         return true;
     }
 
 }
+
+function correctInput(divN){
+    document.getElementById(divN).innerHTML = "Looks good!";
+    document.getElementById(divN).classList.remove('error');
+    document.getElementById(divN).classList.add('correct');
+    
+}
+
+function incorrectInput(divN, message){
+    document.getElementById(divN).innerHTML = message;
+    document.getElementById(divN).classList.remove('correct');
+    document.getElementById(divN).classList.add('error');
+}
+
+function hide(divN){
+    document.getElementById(divN).classList.remove('unhide');
+    document.getElementById(divN).classList.add('hide');
+}
+
+function unhide(divN){
+    document.getElementById(divN).classList.add('unhide');
+    document.getElementById(divN).classList.remove('hide');
+}
+
