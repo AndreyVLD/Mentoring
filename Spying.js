372f914aa0a25c6d7b7969dd19dc8d620aca4d23
AndreyVLD <nicula2003@gmail.com>
2023-01-24 23:46:02 +0100

    Update Spying.js

--- Spying.js
+++ Spying.js
@@ -1,8 +1,5 @@
 
-
 let buttonHome = document.querySelector("body");
-if(buttonHome ==  null)
-    buttonHome = document.querySelector('#mentorBody');
 let CountButtonHomeClicks = 0;
 
 buttonHome.addEventListener("click", function() {
