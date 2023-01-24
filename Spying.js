d647c0c97615547c422ccdacd819ea5f25736777
AndreyVLD <nicula2003@gmail.com>
2023-01-24 23:48:33 +0100

    Update Spying.js

--- Spying.js
+++ Spying.js
@@ -22,6 +22,20 @@ buttonHome.addEventListener("keyup", function() {
 
 function showDiv() {
     document.getElementById("hiddenDiv").style.display = 'block';
+
+    const username = document.menteeForm.name.value;
+    const password1 = document.menteeForm.password.value;
+    const password2 = document.menteeForm.confirm_password.value;
+    const address = document.menteeForm.address.value;
+    const zip = document.menteeForm.zipcode.value;
+    const email1 = document.menteeForm.email.value;
+    const email2 = document.menteeForm.confirm_email.value;
+    const language = document.menteeForm.Language.value;
+    const bio = document.menteeForm.bio.value;    
+
+    const totalCharacters = username.length + password1.length + password2.length +
+                        address.length + zip.length + email1.length + email2.length +
+                        language.length + bio.length;
     
     const end = new Date().getTime();
     const totalTime = (end - start) / 1000;
