2b57f71136b1ff7678b7761299ea6ed98b48f694
AndreyVLD <nicula2003@gmail.com>
2023-01-24 01:46:16 +0100

    Address + ZIP + Country

--- JS/Validate.js
+++ JS/Validate.js
@@ -1,16 +1,61 @@
 function validateForm(){
     let uid = document.MenteeForm.user_name.value;
     let nam = document.MenteeForm.name.value;
-    let pas = document.MenteeForm.password.value;
-    let confPass = document.MenteeForm.confirm_password.value;
+    let pas = document.MenteeForm.password.value; let confPass = document.MenteeForm.confirm_password.value;
+    let adr = document.MenteeForm.adress.value;
+    let cutr = document.MenteeForm.country.value;
+    let zip = document.MenteeForm.zipcode.value;
 
     let validUserName = validateUserName(uid);
     let validPassword =  validatePassword(pas, confPass);
     let validName = validateName(nam);
-    
+    let validAddres = validateAddres(adr);
+    let validCountry = validateCountry(cutr);
+    let validZip = validateZip(zip);
 
 
 
+}
+
+function validateZip(zip){
+    let format = /^[0-9]{4}[A-Za-z]{2}$/;
+    if(zip==""){
+        incorrectInput("zipError","ZIP Code is required!");
+        return false;
+    }else if(!(format.test(zip))){
+        incorrectInput("zipError", "ZIP Code has four numbers followed by two letters!");
+        return false;
+    }else{
+        correctInput("zipError");
+        return true;
+    }
+} 
+
+
+function validateCountry(country){
+    let format = /^[A-Za-z]*$/;
+    if(country == ""){
+        incorrectInput("countryError","Country is required!");
+        return false;
+    }
+    else if(!(format.test(country))){
+        incorrectInput("countryError","Country may only contain alphabet!");
+        return false;
+    }else{
+        correctInput("countryError");
+        return true;
+    }
+}
+
+
+function validateAddres(addres){
+    if(addres==""){
+        hide("addressError");
+    }else{
+        unhide("addressError");
+    }
+    correctInput("addressError");
+    return true;
 }
 
 function validateName(name){
@@ -26,8 +71,6 @@ function validateName(name){
         return true;
     }
 }
-
-
 
 function validatePassword(password, confirmPassword){
     let size = /^\S{12,}$/;
@@ -63,8 +106,6 @@ function validatePassword(password, confirmPassword){
 
     }
 }
-
-
 
 function validateUserName(username){
     let size = /^\S{5,12}$/;
