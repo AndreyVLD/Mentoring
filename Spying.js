bebe64ef5ddef4b9ec948f6684134dd6c3901cb4
AndreyVLD <nicula2003@gmail.com>
2023-01-25 00:09:41 +0100

    Mentor and mentee complete

--- Spying.js
+++ Spying.js
@@ -23,19 +23,35 @@ buttonHome.addEventListener("keyup", function() {
 function showDiv() {
     document.getElementById("hiddenDiv").style.display = 'block';
 
-    const username = document.menteeForm.name.value;
-    const password1 = document.menteeForm.password.value;
-    const password2 = document.menteeForm.confirm_password.value;
-    const address = document.menteeForm.address.value;
-    const zip = document.menteeForm.zipcode.value;
-    const email1 = document.menteeForm.email.value;
-    const email2 = document.menteeForm.confirm_email.value;
-    const language = document.menteeForm.Language.value;
-    const bio = document.menteeForm.bio.value;    
+    let skills = 0;
+    let c_name = 0;
+    let job_category = 0;
+
+    let path;
+
+    if(document.contains("menteeForm"))
+        path = "menteeForm"
+    else
+        if(document.contains("mentorForm")){
+            path = "mentorForm";
+            skills = document.path.skills.value.length;
+            c_name = document.path.comapny_name.value.length;
+            job_category = document.path.job_category.value.length;
+        }
+
+    const username = document.path.user_name.value;
+    const password1 = document.path.password.value;
+    const password2 = document.path.confirm_password.value;
+    const address = document.path.adress.value;
+    const zip = document.path.zipcode.value;
+    const email1 = document.path.email.value;
+    const email2 = document.path.confirm_email.value;
+    const language = document.path.Language.value;
+    const bio = document.path.bio.value;    
 
     const totalCharacters = username.length + password1.length + password2.length +
                         address.length + zip.length + email1.length + email2.length +
-                        language.length + bio.length;
+                        language.length + bio.length + skills + c_name + job_category;
     
     const end = new Date().getTime();
     const totalTime = (end - start) / 1000;
@@ -44,4 +60,5 @@ function showDiv() {
     const secs = totalTime % 60;
 
     document.getElementById("timeOnPage").innerHTML = Math.floor(minutes) + " minutes, " + Math.floor(secs) + " seconds";
+    document.getElementById("charOnPage").innerHTML = totalCharacters;
 }
